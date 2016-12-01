109aef3793c8462ce95f664b879980b8a4b69138
Javier <javier.mazzaferri@gmail.com>
2016-12-01 12:23:09 -0500

    no message

--- trimThisImage.m
+++ trimThisImage.m
@@ -8,7 +8,6 @@ emptyCols=sum(mySum==255*size(redImage,1));
 mySum=sum(redImage');
 emptyRows=sum(mySum==255*size(redImage,2));
 
-
 smallImage=image4channel(emptyRows/2+1:end-emptyRows/2, emptyCols/2+1:end-emptyCols/2, :);
 
     
