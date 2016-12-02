2dbc9419a477b4c780bf99468abcbfaa7336e72a
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2016-12-02 10:37:14 -0500

    Reincorporé el cambio de código para que podamos correr el código los dos.

--- analyzeThisFolder.m
+++ analyzeThisFolder.m
@@ -1,6 +1,11 @@
 clear
 %% Set folders
 masterFolder='/Users/santiago/Dropbox (Biophotonics)/Projects/Bruno/Images/ToTest/Anonymous/';
+
+% Change folder if Javier
+[~,user] = system('whoami');
+if strcmp(strtrim(user),'javimazzaf'), masterFolder='../Anonymous/';end
+
 warning('Off')
 mkdir(masterFolder, 'Masks')
 mkdir(masterFolder, 'TuftImages')
