41ff76bb41395cda2688bc7cc94385beecae2d73
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2016-12-16 15:23:43 -0500

    Simple update of files

--- optimizationLoop.m
+++ optimizationLoop.m
@@ -9,10 +9,10 @@ try
     inifile('config.ini','write',{'','','doVasculature',0})
     inifile('config.ini','write',{'','','doSaveImages',0})
     
-    paramName = 'tufts.bright.DilatingRadiusDivisor';
-    N      = 5;
-    minVal = 1000;
-    maxVal = 10000;
+    paramName = 'tufts.bright.OpeningSizeDivisor1';
+    N      = 9;
+    minVal = 50;
+    maxVal = 950;
     parValues = ((1:N)-1)/(N-1) * (maxVal-minVal) + minVal;
     
     allTP = NaN(1,N);
