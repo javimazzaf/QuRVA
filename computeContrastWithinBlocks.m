091583ad23eaa13be4d930ea317d151b099d129e
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-05-24 11:34:13 -0400

    Simple

---
+++ computeContrastWithinBlocks.m
@@ -0,0 +1,18 @@
+function features = computeContrastWithinBlocks(inIm,blocksInd,selBlocks, offSet)
+
+features = zeros(size(selBlocks,1),1);
+
+sz       = size(blocksInd);
+bSz      = sz(1:2);
+nBlocks  = sz(3:4);
+
+padSz   = nBlocks .* bSz - size(inIm) + offSet;
+    
+inIm = padarray(inIm,padSz,0,'post');
+
+for k = 1:size(selBlocks,1)
+    aux = inIm(blocksInd(:,:,selBlocks(k,1),selBlocks(k,2)));
+    features(k) = mean(aux(:));
+end
+
+end
