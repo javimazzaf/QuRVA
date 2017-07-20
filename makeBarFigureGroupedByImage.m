f3d965b991466ff4869818f02de3a3bfbedfde5a
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-07-20 13:33:08 -0400

    Simple Update

--- makeBarFigureGroupedByImage.m
+++ makeBarFigureGroupedByImage.m
@@ -10,21 +10,21 @@ hold on
 
 
 for it=2:size(data, 1)
-    set(bar1(it),'FaceColor',[0 2/9+it*1/9 0], 'DisplayName',users{it-1});
+    set(bar1(it),'FaceColor',[0 2/9+it*1/9 0],'EdgeColor',[0 2/9+it*1/9 0], 'DisplayName',users{it-1});
 end
 
 for it=1:size(data, 2)
     xEtiquetas{it}=['Image ' num2str(it)];
     
-    % Compute users mean and std for each image
-    aux = data(2:end,it);
-    md(it) = median(aux);
-    sd(it) = std(aux);
-    
-    plot(it + ((0:size(data,1)-1) - floor(size(data,1)/2)) / size(data,1) * 0.5, md(it) * ones(size(data(:,it))), 'Color', 'm','linewidth',2)
-    
-    plot(it + ((0:size(data,1)-1) - floor(size(data,1)/2)) / size(data,1) * 0.5, (md(it) - sd(it)) * ones(size(data(:,it))), 'Color', 'b','linewidth',2)
-    plot(it + ((0:size(data,1)-1) - floor(size(data,1)/2)) / size(data,1) * 0.5, (md(it) + sd(it)) * ones(size(data(:,it))), 'Color', 'b','linewidth',2)
+%     % Compute users mean and std for each image
+%     aux = data(2:end,it);
+%     md(it) = median(aux);
+%     sd(it) = std(aux);
+%     
+%     plot(it + ((0:size(data,1)-1) - floor(size(data,1)/2)) / size(data,1) * 0.5, md(it) * ones(size(data(:,it))), 'Color', 'm','linewidth',2)
+%     
+%     plot(it + ((0:size(data,1)-1) - floor(size(data,1)/2)) / size(data,1) * 0.5, (md(it) - sd(it)) * ones(size(data(:,it))), 'Color', 'b','linewidth',2)
+%     plot(it + ((0:size(data,1)-1) - floor(size(data,1)/2)) / size(data,1) * 0.5, (md(it) + sd(it)) * ones(size(data(:,it))), 'Color', 'b','linewidth',2)
     
 end
 
