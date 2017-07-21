3f1b240e968f0ce1f631572e80ba615be74d542c
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-07-21 12:52:10 -0400

    Simple Update

--- createModels.m
+++ createModels.m
@@ -3,7 +3,7 @@ readConfig
 load(fullfile(masterFolder, 'trainingSet.mat'),'data1','res1','data2','res2','versionInfo')
 trainingSetVersInfo = versionInfo;
 
-model1 = fitcdiscr(data1,res1,'DiscrimType','quadratic','Cost',tufts.classCost,'Prior','empirical');
-model2 = fitcdiscr(data2,res2,'DiscrimType','quadratic','Cost',tufts.classCost,'Prior','empirical');
+model1 = fitcdiscr(data1,res1,'DiscrimType','quadratic','Cost',tufts.classCost,'Prior','uniform');
+model2 = fitcdiscr(data2,res2,'DiscrimType','quadratic','Cost',tufts.classCost,'Prior','uniform');
 
 save(fullfile(masterFolder, 'model.mat'),'model1','model2','trainingSetVersInfo','-v7.3')
