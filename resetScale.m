e1943da446cfc015f412b61809ab3078ede1e08e
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-07-24 15:55:32 -0400

    Simple update of files

--- resetScale.m
+++ resetScale.m
@@ -1,15 +1,15 @@
 function [outImage, scaleFactor] = resetScale(inImage)
 
 sz = size(inImage);
-    
-    [~,ix] = max(sz);
-    
-    newSize = [NaN NaN];
-    newSize(ix) = 4096;
-    
-    outImage = imresize(inImage,newSize);
-    
-    outSz=size(outImage);
-    scaleFactor=sz(1)/outSz(1);
-    
-    end
+
+[~,ix] = max(sz);
+
+newSize = [NaN NaN];
+newSize(ix) = 4096;
+
+outImage = imresize(inImage,newSize);
+
+outSz=size(outImage);
+scaleFactor=sz(1)/outSz(1);
+
+end
