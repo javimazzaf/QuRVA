cc7579d9b273e76c6453a0f7686c9542f18c9c43
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-09-13 09:01:53 -0400

    Simple update of files

--- testSwiftTraining.m
+++ testSwiftTraining.m
@@ -1,17 +1,25 @@
 %Training a model using several swift images from Bertan 
 
-imPath = '/Volumes/EyeFolder/Dropbox (Biophotonics)/Deep_learning_Images/OIR/raw/';
+basePath = '/Volumes/EyeFolder/';
+
+imPath = fullfile(basePath,'Dropbox (Biophotonics)/Deep_learning_Images/OIR/raw/');
 
 maskFiles = dir(fullfile(imPath,'Masks','*.mat'));
 maskFiles = {maskFiles(:).name};
 
 allFiles = cellfun(@(x) x(1:end-4),maskFiles,'UniformOutput',false);
 
-trainPath = '/Volumes/EyeFolder/Dropbox (Biophotonics)/Deep_learning_Images/OIR/swift/';
+trainPath = fullfile(basePath,'Dropbox (Biophotonics)/Deep_learning_Images/OIR/swift/');
 
-modelDir = '/Volumes/EyeFolder/Dropbox (Biophotonics)/Deep_learning_Images/OIR/trainingSwift/';
+modelDir = fullfile(basePath, 'Dropbox (Biophotonics)/Deep_learning_Images/OIR/trainingSwift/');
 if ~exist(modelDir,'dir')
     mkdir(modelDir);
 end
 
-trainQuRVA(imPath,trainPath,allFiles(1:50),fullfile(modelDir,'trainingSet.mat'),fullfile(modelDir,'model.mat'))
+train = true;
+
+if train
+   trainQuRVA(imPath,trainPath,allFiles(1:50),fullfile(modelDir,'trainingSet.mat'),fullfile(modelDir,'model.mat'))
+else
+   processFolder(fullfile(imPath,allFiles(51:end))); 
+end
