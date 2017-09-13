3256fb0559452c438744000d8d436b06279cc122
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-09-13 09:15:17 -0400

    Simple update of files

--- testSwiftTraining.m
+++ testSwiftTraining.m
@@ -1,6 +1,10 @@
 %Training a model using several swift images from Bertan 
 
-basePath = '/Volumes/EyeFolder/';
+if ismac
+   basePath = '/Volumes/EyeFolder/';
+elseif isunix
+   basePath = '~/';
+end
 
 imPath = fullfile(basePath,'Dropbox (Biophotonics)/Deep_learning_Images/OIR/raw/');
 
@@ -16,7 +20,7 @@ if ~exist(modelDir,'dir')
     mkdir(modelDir);
 end
 
-train = true;
+train = false;
 
 if train
    trainQuRVA(imPath,trainPath,allFiles(1:50),fullfile(modelDir,'trainingSet.mat'),fullfile(modelDir,'model.mat'))
