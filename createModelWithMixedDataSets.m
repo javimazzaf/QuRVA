3ee72dc6482bd716f839618f966ca85cf6c5227f
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-09-13 12:09:53 -0400

    Simple update of files

--- createModelWithMixedDataSets.m
+++ createModelWithMixedDataSets.m
@@ -2,9 +2,13 @@ function createModelWithMixedDataSets
 
 readConfig
 
-load(fullfile(masterFolder, 'trainingSet.mat'),'data','res')
-% trainingSetVersInfo = versionInfo;
+swiftVars = load(fullfile('../', 'trainingSetSwift.mat'),'data','res');
+
+our20Vars = load(fullfile('../', 'trainingSet20ours.mat'),'data','res');
+
+data = [swiftVars.data;our20Vars.data];
+res = [swiftVars.res;our20Vars.res];
 
 model = fitcdiscr(data,res,'DiscrimType','quadratic','Cost',tufts.classCost,'Prior','empirical');
 
-save(fullfile(masterFolder, 'model.mat'),'model','-v7.3')
+save(fullfile('../', 'modelAll.mat'),'model','-v7.3')
