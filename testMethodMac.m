553692e78a452f27b92df3aff56e418603bc781e
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-09-27 10:39:32 -0400

    Simple update of files

--- testMethodMac.m
+++ testMethodMac.m
@@ -1,5 +1,5 @@
-% imPath = '../Anonymous/';
-imPath = '/Users/javimazzaf/Dropbox (Biophotonics)/Francois/310117TOTM/';
+imPath = '../Anonymous/';
+% imPath = '/Users/javimazzaf/Dropbox (Biophotonics)/Francois/310117TOTM/';
 
 
 maskFiles = dir(fullfile(imPath,'Masks','*.mat'));
@@ -7,20 +7,20 @@ maskFiles = {maskFiles(:).name};
 
 allFiles = cellfun(@(x) x(1:end-4),maskFiles,'UniformOutput',false);
 
-% processFolder(imPath,allFiles(21:2:end)); 
+processFolder(imPath,allFiles(21:2:end)); 
 
-rawImage = imread(fullfile(imPath,allFiles{4}));
-load(fullfile(imPath,'Masks',maskFiles{4}));
-
-rawImage = resetScale(rawImage(:,:,1));
-
-thisMask = resetScale(thisMask);
-
-[bgMean,bgStd] = getRobustLocalBackground(rawImage, thisMask);
-
-rawImage = rawImage.*uint8(thisMask);
-rawImageNorm = mat2gray(double(rawImage));
-
-mask = (rawImageNorm > (bgMean + 3*bgStd));
-
-figure;imshow(mask & thisMask,[])
+% rawImage = imread(fullfile(imPath,allFiles{4}));
+% load(fullfile(imPath,'Masks',maskFiles{4}));
+% 
+% rawImage = resetScale(rawImage(:,:,1));
+% 
+% thisMask = resetScale(thisMask);
+% 
+% [bgMean,bgStd] = getRobustLocalBackground(rawImage, thisMask);
+% 
+% rawImage = rawImage.*uint8(thisMask);
+% rawImageNorm = mat2gray(double(rawImage));
+% 
+% mask = (rawImageNorm > (bgMean + 3*bgStd));
+% 
+% figure;imshow(mask & thisMask,[])
