aa5e7e3cc377b38e0ecebe5f4690505fbcbd422e
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-10-04 12:54:45 -0400

    Simple update of files

---
+++ computeErrorsUsers.m
@@ -0,0 +1,54 @@
+% loads local parameters
+readConfig;
+
+%Ensures everything is commited before starting test.
+[versionInfo.branch, versionInfo.sha] = getGitInfo;
+
+mkdir(masterFolder, 'Global')
+
+%% Get file names
+myFiles = dir(fullfile(masterFolder, 'TuftNumbers','*.mat'));
+myFiles = {myFiles(:).name};
+
+%% Load results
+
+for it=1:numel(myFiles)
+    disp(myFiles{it});
+    
+    load(fullfile(masterFolder, 'TuftNumbers', myFiles{it}),'tuftsMask');
+    load(fullfile(masterFolder, 'TuftConsensusMasks',myFiles{it}),'allMasks')
+    allMasks = resetScale(allMasks);
+    
+    consensusMask = sum(allMasks, 3) >= consensus.reqVotes;
+    
+    load(fullfile(masterFolder,'SwiftMasks',myFiles{it}),'swiftMasks')
+    swiftMasks = resetScale(swiftMasks);
+    
+    % Each Evaluator
+    for itUsers=1:size(allMasks,3)
+        FPothers(itUsers+1,it) = sum(sum(allMasks(:,:,itUsers) > consensusMask));
+        FNothers(itUsers+1,it) = sum(sum(allMasks(:,:,itUsers) < consensusMask));
+    end
+    
+    % Each Swift
+    for itSwift = 1:size(swiftMasks,3)
+        
+        %Check if I got zeros because the user did not analyze this image
+        if max(max(swiftMasks(:,:,itSwift))) == 0
+            FPothers(itUsers+1+itSwift,it) = NaN;
+            FNothers(itUsers+1+itSwift,it) = NaN;
+        else
+            FPothers(itUsers+1+itSwift,it) = sum(sum(swiftMasks(:,:,itSwift) > consensusMask));
+            FNothers(itUsers+1+itSwift,it) = sum(sum(swiftMasks(:,:,itSwift) < consensusMask));  
+        end
+        
+    end
+
+end
+
+save(fullfile(masterFolder,'comparisonOthers.mat'),'FPothers','FNothers','versionInfo')
+
+
+
+
+
--- resetScale.m
+++ resetScale.m
@@ -7,9 +7,24 @@ sz = size(inImage);
 newSize = [NaN NaN];
 newSize(ix) = 4096;
 
-outImage = imresize(inImage,newSize);
+layer1   = imresize(inImage(:,:,1),newSize);
 
-outSz=size(outImage);
+if size(sz) == 3
+    
+    outImage = zeros([size(layer1) sz(3)]);
+    outImage(:,:,1) = layer1;
+    
+    for k = 2:sz(3)
+        outImage(:,:,k) = imresize(inImage(:,:,k),newSize);
+    end
+    
+else
+    
+    outImage = layer1;
+    
+end
+
+outSz=size(layer1);
 scaleFactor=sz(1)/outSz(1);
 
 end
