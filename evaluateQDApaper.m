4b2baeab292e0427c2eab57e161adda89b6908d8
Javier Mazzaferri <javier.mazzaferri@gmail.com>
2017-10-05 15:01:11 -0400

    Simple Update

--- evaluateQDApaper.m
+++ evaluateQDApaper.m
@@ -7,6 +7,7 @@ myFiles = {myFiles(:).name};
 
 myFiles = cellfun(@(x) x(1:end-4),myFiles,'UniformOutput',false);
 
-processFolder(masterFolder,myFiles(8:14),model1,true);
 processFolder(masterFolder,myFiles(1:7), model2,true);
+processFolder(masterFolder,myFiles(8:14),model1,true);
+
 
